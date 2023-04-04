9f846576a734e9b5f61b56a96be1c226acd869ad
ShaTeixeira <shayaneteixeira56@gmail.com>
2023-04-04 19:37:17 -0300

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,29 @@
+const UserController = require('./controllers/UseController')
+
+module.exports = [
+  {
+    endpoint: '/users',
+    method: 'GET',
+    handler: UserController.listUsers,
+  },
+  {
+    endpoint: '/users/:id', /* :id -> placeholder indicando que depois vem um ID */
+    method: 'GET',
+    handler: UserController.getUserById,
+  },
+  {
+    endpoint: '/users',
+    method: 'POST',
+    handler: UserController.createUser,
+  },
+  {
+    endpoint: '/users/:id',
+    method: 'PUT',
+    handler: UserController.updateUser,
+  },
+  {
+    endpoint: '/users',
+    method: 'POST',
+    handler: UserController.createUser,
+  },
+]
